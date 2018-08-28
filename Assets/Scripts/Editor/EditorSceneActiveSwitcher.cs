e97b8de210b0fac9f4829f80d053622f782cf924
takupisu <ni.ka.janne@gmail.com>
2018-08-28 15:35:39 +0900

    first

---
+++ Assets/Scripts/Editor/EditorSceneActiveSwitcher.cs
@@ -0,0 +1,58 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.Reflection;
+
+/// <summary>
+/// Hierarchy上にチェックボックスでシーンのLoad、UnLoad状態を切り替えれる機能
+/// </summary>
+[InitializeOnLoad]
+public class EditorSceneSwitcher : Editor {
+
+	static EditorSceneSwitcher () {
+		EditorApplication.hierarchyWindowItemOnGUI += (instanceID, rect) => {
+			DrawComponentIcons (instanceID, rect);
+		};
+	}
+
+	static void DrawComponentIcons (int instanceID, Rect rect)
+	{
+		if(Application.isPlaying) {
+			return;
+		}
+
+		// チェックボックス表示位置
+		rect.x += rect.width - 40;
+		rect.width = 40;
+
+		if(EditorUtility.InstanceIDToObject(instanceID)) {
+			return;
+		}
+
+		var setups = EditorSceneManager.GetSceneManagerSetup();
+		if(null == setups || setups.Length <= 0) {
+			return;
+		}
+
+		var miGetSceneByHandle = typeof(EditorSceneManager).GetMethod("GetSceneByHandle",BindingFlags.NonPublic | BindingFlags.Static);
+		Scene s = (Scene)miGetSceneByHandle.Invoke(null, new object[]{instanceID});
+
+		if (s.isLoaded != GUI.Toggle(rect,s.isLoaded,"")){
+			for (int i = 0; i < setups.Length; i++){
+				if (setups[i].path == s.path){
+					setups[i].isLoaded = !s.isLoaded;
+					if(s.isLoaded) {
+						EditorSceneManager.SaveScene(s);
+						EditorSceneManager.CloseScene(s, false);
+					} else {
+						EditorSceneManager.OpenScene(s.path, OpenSceneMode.Additive);
+					}
+					break;
+				}
+			}
+		}
+	}
+}
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # EditorSceneActiveSwitcher-Unity
-Hierarchy上にチェックボックスを設置しそれでシーンのLoad、UnLoad状態を切り替えれる機能
+Hierarchy上のチェックボックスでシーンのLoad、UnLoad状態を切り替える機能
+
+It established a check box on the Hierarchy So the scene Load functions, is switching the UnLoad state
+
+![Demo](https://github.com/VENIEGAMES/EditorSceneActiveSwitcher-Unity/blob/master/demo.gif "Demo")
